ffc44a16c1734c882fdb753d00d11d15d9db9d1b
Мартынюк Максим <martiniuc.maxim1999@gmail.com>
2020-11-04 14:08:47 +0200

    addet in route.yaml new path for URL

--- src/Controller/LuckyController.php
+++ src/Controller/LuckyController.php
@@ -3,8 +3,19 @@
 
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class LuckyController
 {
+ public function number()
+   {
+       /*
+        * @Route("/lucky/number)
+        */
+       $number = mt_rand(0,100);
+
+       return new Response('<html><body> Lucky number is:'.$number.'</body></html>');
+   }
 
 }
